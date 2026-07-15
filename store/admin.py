bae4c623530d1f041a2d017c9579507a5b50644d
Hamoun <hamounbamoradi@gmail.com>
2026-07-15 19:35:48 +0330

    test v1.0.0

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,101 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(DateModel)
+class DateAdmin(admin.ModelAdmin):
+    readonly_fields = ("user_created", "user_modified", "created_date", "modified_date")
+    search_fields = ("year", "month", "day")
+    
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.user_modified = request.user
+        else:
+            obj.user_created = request.user
+            obj.user_modified = request.user
+        return super().save_model(request, obj, form, change)
+    
+
+@admin.register(CustomerModel)
+class CustomerAdmin(admin.ModelAdmin):
+    readonly_fields = ("user_created", "user_modified", "created_date", "modified_date")
+    search_fields = ("code", "name")
+    
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.user_modified = request.user
+        else:
+            obj.user_created = request.user
+            obj.user_modified = request.user
+        return super().save_model(request, obj, form, change)
+    
+
+@admin.register(ProductModel)
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ("user_created", "user_modified", "created_date", "modified_date")
+    search_fields = ("code", "name")
+    
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.user_modified = request.user
+        else:
+            obj.user_created = request.user
+            obj.user_modified = request.user
+        return super().save_model(request, obj, form, change)
+    
+
+@admin.register(SaleModel)
+class SaleAdmin(admin.ModelAdmin):
+    readonly_fields = ("user_created", "user_modified", "created_date", "modified_date")
+    search_fields = ("date", "customer", "product", "price_total")
+    
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.user_modified = request.user
+        else:
+            obj.user_created = request.user
+            obj.user_modified = request.user
+        return super().save_model(request, obj, form, change)
+    
+
+@admin.register(StoreModel)
+class StoreAdmin(admin.ModelAdmin):
+    readonly_fields = ("user_created", "user_modified", "created_date", "modified_date")
+    search_fields = ("date", "product", "sale_group")
+    
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.user_modified = request.user
+        else:
+            obj.user_created = request.user
+            obj.user_modified = request.user
+        return super().save_model(request, obj, form, change)
+    
+
+@admin.register(CategoryFileModel)
+class CategoryFileAdmin(admin.ModelAdmin):
+    readonly_fields = ("user_created", "user_modified", "created_date", "modified_date")
+    search_fields = ("name", )
+    
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.user_modified = request.user
+        else:
+            obj.user_created = request.user
+            obj.user_modified = request.user
+        return super().save_model(request, obj, form, change)
+    
+
+@admin.register(FileModel)
+class FileAdmin(admin.ModelAdmin):
+    readonly_fields = ("user_created", "user_modified", "created_date", "modified_date")
+    search_fields = ("date", "category")
+    
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.user_modified = request.user
+        else:
+            obj.user_created = request.user
+            obj.user_modified = request.user
+        return super().save_model(request, obj, form, change)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django import views
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.db.models import Q
+from django.contrib import messages
+from .models import *
 
 # Create your views here.
+
+class Test(PermissionRequiredMixin, views.View):
+    login_url = 'accounts:signin'
+    permission_required = ['crm.add_customermodel', 'crm.add_requestmodel']
+
+    def get(self, request):
+        return render(request, 'staff/test.html')
+
+
+class CustomerListView(PermissionRequiredMixin, views.View):
+    login_url = "accounts:signin"
+    permission_required = ["store.view_customermodel"]
+
+    def get(self, request):
+        customer = CustomerModel.objects.all()
+        context = {
+            "customer":customer,
+        }
+        return render(request, "store/customer-list.html", context)
+    
+
+class DateListView(PermissionRequiredMixin, views.View):
+    login_url = "accounts:login"
+    permission_required = ["store.view_datemodel"]
+
+    def get(self, request):
+        date = DateModel.objects.all()
+        context = {
+            "date":date,
+        }
+        return render(request, "store/date-list.html", context)
+    
+
+class ProductListView(PermissionRequiredMixin, views.View):
+    login_url = "accounts:login"
+    permission_required = ["store.view_productmodel"]
+
+    def get(self, request):
+        product = ProductModel.objects.all()
+        context = {
+            "product":product,
+        }
+        return render(request, "store/product-list.html")
